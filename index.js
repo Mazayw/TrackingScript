fd4b0362ade13a67a425c290ec3dae5568a34876
unknown <mazayw@gmail.com>
2023-05-04 12:06:00 +0300

    feat: fix cors

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
 // ==UserScript==
 // @name         Tracking Script for Sborka
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.8
 // @author       Mazayw
 // @match        https://sborka.ua/?id=80*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sborka.ua
 // @downloadURL  https://raw.githubusercontent.com/Mazayw/TrackingScript/master/index.js?token=GHSAT0AAAAAABTFORFDZYVYA4JLKXJ7CNHQYUDVAQA
 // @updateURL    https://raw.githubusercontent.com/Mazayw/TrackingScript/master/index.js?token=GHSAT0AAAAAABTFORFDZYVYA4JLKXJ7CNHQYUDVAQA
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function () {
-  'use strict';
-  const data = {
-    apiKey: '[ВАШ КЛЮЧ]',
-    modelName: 'TrackingDocument',
-    calledMethod: 'getStatusDocuments',
-    methodProperties: {
-      Documents: [
-        {
-          DocumentNumber: '20400281751880',
-          Phone: '380500000001',
-        },
-      ],
-      CheckWeightMethod: '',
-      CalculatedWeight: '3',
-    },
-  };
+	'use strict';
+	const data = (DocumentNumber) => ({
+		modelName: 'TrackingDocument',
+		calledMethod: 'getStatusDocuments',
+		methodProperties: {
+			Documents: [
+				{
+					DocumentNumber,
+					Phone: '380500000001',
+				},
+			],
+			CheckWeightMethod: '',
+			CalculatedWeight: '3',
+		},
+	});
 
-  const url = 'https://api.novaposhta.ua/v2.0/json/';
+	const handleResponse = (elem) => (response) => {
+		const data = JSON.parse(response.response).data[0];
+		console.log(data);
 
-  const trackingNumbers = document.getElementsByClassName('tracking-parcel');
-  for (const elem of trackingNumbers) {
-    data.methodProperties.Documents[0].DocumentNumber =
-      elem.textContent.slice(6);
+		elem.innerText += '\n' + data.Status;
+		if (data.DateReturnCargo) {
+			const daySave = Math.ceil(
+				(Date.parse(data.DateReturnCargo) - Date.now()) / (1000 * 3600 * 24)
+			);
+			elem.innerText += `\n Залишилось ${daySave} днів`;
+			if (daySave < 4) elem.style.color = 'red';
+		}
+		if (data.ExpressWaybillAmountToPay > 0) {
+			elem.innerText += `\n До оплати ${data.ExpressWaybillAmountToPay} грн`;
+		}
+	};
 
-    fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.data[0]);
-        elem.innerText += '\n' + data.data[0].Status;
-        if (data.data[0].DateReturnCargo) {
-          const daySave = Math.ceil(
-            (Date.parse(data.data[0].DateReturnCargo) - Date.now()) /
-              (1000 * 3600 * 24)
-          );
-          elem.innerText += `\n Залишилось ${daySave} днів`;
-          if (daySave < 4) elem.style.color = 'red';
-        }
-        if (data.data[0].ExpressWaybillAmountToPay > 0) {
-          elem.innerText += `\n До оплати ${data.data[0].ExpressWaybillAmountToPay} грн`;
-        }
-      });
-  }
+	const url = 'https://api.novaposhta.ua/v2.0/json/';
+
+	const trackingNumbers = document.getElementsByClassName('tracking-parcel');
+	for (const elem of trackingNumbers) {
+		GM_xmlhttpRequest({
+			method: 'POST',
+			url,
+			data: JSON.stringify(data(elem.textContent.slice(6))),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			onload: handleResponse(elem),
+		});
+	}
 })();
